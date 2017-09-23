28402f7d96663315172b220a8c3374a6bf770867
Unknown <wasuradananjith@gmail.com>
2017-09-23 13:06:55 +0530

    Google Map Activity Added

--- app/src/main/java/com/example/wasuradananjith/mydoctor/HomeActivity.java
+++ app/src/main/java/com/example/wasuradananjith/mydoctor/HomeActivity.java
@@ -1,13 +1,40 @@
 package com.example.wasuradananjith.mydoctor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    Button nearDoc,vidCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        // Construct a FusedLocationProviderClient.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        nearDoc = (Button)findViewById(R.id.btnNearDoc);
+        vidCall = (Button)findViewById(R.id.btnVidCall);
+
+        nearDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, NearDoctorActivity.class));
+                overridePendingTransition(R.anim.slide_out_top,R.anim.slide_in_bottom);
+                finish();
+            }
+        });
     }
+
 }
